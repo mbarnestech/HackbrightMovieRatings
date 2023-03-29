9651e35ea8e9c7eaf551e9ae5866eff73e8ae407
mbarnestech <maggie.barnes@gmail.com>
2023-03-29 15:02:46 -0600

    define create_movie function

--- crud.py
+++ crud.py
@@ -2,6 +2,7 @@
 
 # import functions from model.py
 from model import db, User, Movie, Rating, connect_to_db
+from datetime import datetime
 
 # CRUD functions
 def create_user(email, password):
@@ -11,9 +12,17 @@ def create_user(email, password):
 
     return user
 
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
     
+    movie = Movie(
+                title = title, 
+                overview = overview, 
+                release_date = release_date, 
+                poster_path = poster_path
+                )
 
-
+    return movie
 
    
     
